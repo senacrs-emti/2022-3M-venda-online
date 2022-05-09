5e919594df6b10bd680945acd085b779a5bb5e51
Fah <80393387+Hollow-Soul@users.noreply.github.com>
2022-05-09 09:21:21 -0300

    nav-bar

--- index.php
+++ index.php
@@ -18,25 +18,25 @@
 
   <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
     <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(página atual)</span></a>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Ingressos <span class="sr-only">(página atual)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
+        <a class="nav-link" href="#">Sobre</a>
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
+          Para Empresa
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Ação</a>
-          <a class="dropdown-item" href="#">Outra ação</a>
+          <a class="dropdown-item" href="#">Cadeiras</a>
+          <a class="dropdown-item" href="#">Camarotes</a>
           <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Algo mais aqui</a>
+          <a class="dropdown-item" href="#">Sua marca na Arena</a>
         </div>
       </li>
       <li class="nav-item">
-        <a class="nav-link disabled" href="#">Desativado</a>
+        <a class="nav-link" href="#">Notícias</a>
       </li>
     </ul>
     <form class="form-inline my-2 my-lg-0">
