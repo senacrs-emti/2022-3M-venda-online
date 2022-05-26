1d45d97e21176bdec29601e84e7b5f6511e93af2
guilherme204 <103538400+guilherme204@users.noreply.github.com>
2022-05-26 14:53:04 -0300

    front

--- index.php
+++ index.php
@@ -9,7 +9,6 @@
     <title>Home</title>
 </head>
 <body style="background-color: #354e7c">
-
 <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #000000">
   <a class="navbar-brand" href="index.php">Base Do Grêmio</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
@@ -61,18 +60,13 @@
     
     </section>
   
-
-
-
-
-
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </body>
 <footer>
   <div class="logotipo">
-    <img src="imagens/logotipo.png">
+    <img src="imagens/arena.png">
   </div>
   
 
