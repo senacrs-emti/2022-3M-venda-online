087a128002a1079b2e6251cbfa502633fd5e3ba3
guilherme204 <103538400+guilherme204@users.noreply.github.com>
2022-06-01 08:27:56 -0300

    favicon

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
     <title>Base do Grêmio</title>
+    <link rel="shortcut icon" type="image/x-icon" href="imagens/gremio.png">
+
 </head>
 <body style="background-color: #354e7c">
 <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #000000">
