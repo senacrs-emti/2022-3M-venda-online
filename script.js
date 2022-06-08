c9413dd88a142152643ca0b9f1a500a56c5c9e43
Vicenzo Frusciante <81040684+VicenzoMF@users.noreply.github.com>
2022-06-08 10:27:52 -0300

    ajax

--- script.js
+++ script.js
@@ -1,23 +1,52 @@
-$(document).ready(function(){
-    $("#popupUser").click(function(){
-        
-      $.ajax({
-          type: "POST",
-          url: "cadastro.php", 
-          success: function(result){
+  // http://devfuria.com.br/javascript/ajax-php-jquery/
 
-            alert('ahaaaa clicou não dorme Vicenzo');
-            $("#div1").html(result);
-        }});
-    });
-  });
+  var request = $.ajax({
 
-  $(document).ready(function(){
-    $("#chk").click(function(){
-      $()
-    });
-  });
+    //
+    // A propriedade `url` é local, arquivo, script, alvo de sua requisição.
+    //
+    url: "cadastro.php",
 
+    //
+    // A propriedade `type` é o verbo HTTP (GET, POST, HEAD, etc...)
+    //
+    type: "POST",
+
+    //
+    // A propriedade `data` são os dados de sua aplicação.
+    //
+    data: "campo1=dado1&campo2=dado2&campo3=dado3",
+
+    //
+    // A propriedade `dataType` refere-se ao tipo de dado que o servidor deve retornar a requisição.
+    //
+    dataType: "html"
+});
+
+//
+// O método `done()` recebe uma função de callback
+// que será executada caso a requisição tenha sucesso.
+//
+request.done(function(resposta) {
+    console.log(resposta)
+});
+
+//
+// O método `fail()`recebe uma função de callback
+// que será executada caso a requisição falhe.
+//
+request.fail(function(jqXHR, textStatus) {
+    console.log("Request failed: " + textStatus);
+});
+
+//
+// O método `always()` recebe uma função de callback
+// que será executada quando a requisição de sucesso estiver completa.
+//
+request.always(function() {
+    console.log("completou");
+});
+  
   function transformPopUp()  {
     if (chk.checked == true){
       document.getElementById("popupUser").style.transform = "translateY(230%)"
@@ -26,15 +55,6 @@ $(document).ready(function(){
     }
   }
 
-// ---------- VAI MAS NAO VOLTA -----------
-  // function transformPopUp() {
-  //   let popupUser = document.getElementById('popupUser');
-  //     if (condition) {
-  //     popupUser.style.transform = "translateY(230%)";
-  //     } else {
-  //       popupUser.style.transform = "translateY(-230%)";
-  //     }
-  // }
 
 //   function closePopupUser() {
 //     let popupUser = document.getElementById('popupUser');
