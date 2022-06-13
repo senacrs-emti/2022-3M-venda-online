2b72247c6b45d3ccb0a6b448c9f02d6b35ac7a83
itsmelaura07 <guilhermegil2004@gmail.com>
2022-06-13 09:57:43 -0300

    front-end

---
+++ noticiadois.php
@@ -0,0 +1,41 @@
+<?php 
+include_once 'navbar.php';
+?>
+
+<div id="textoum">
+<h2>Grêmio recebe autoridades públicas para cerimônia de apresentação do projeto social "Futuro Suave do Gramado ao Tatame"</h2>
+</div>
+<div id="textodois">
+<h3>Familiares das crianças e adolescentes participantes também prestigiaram o evento</h3>
+</div>
+<div id="textotres">
+<p> 07 JUN 2022 16:50 | ATUALIZADO EM 07 JUN 2022 17:39</p>
+</div>
+<div id="imagemum">
+<img src="imagens/noticia2.jpg">
+</div>
+
+
+<div id="textoquatro">
+<p>Na tarde desta terça-feira, 07, ocorreu a cerimônia de apresentação do projeto social “Futuro Suave do Gramado ao Tatame.” A solenidade, realizada no saguão da Tribuna de Honra do Conselho Deliberativo da Arena,  foi prestigiada pelo vice-presidente do CD do Grêmio, Alexandre Bugin,  pelo presidente do Conselho de Administração, Romildo Bolzan, pelo vice-presidente Guto Peixoto, pelo diretor do Departamento de Responsabilidade Social do Tricolor, Rodrigo Kandrik, além do Secretário Estadual Adjunto do Esporte e Lazer, Bruno Ortiz, e da Secretária Municipal de Esporte, Lazer e Juventude, Débora Garcia, representantes da Comunidade TRI, executivos do Grêmio e da Arena. </p>  
+
+<p>O campeão mundial e faixa preta de Jiu Jitsu, Nato Werlang, abriu o evento saudando a todos os presentes e contando um pouco da história que deu início à iniciativa: “Consegui, com a força do esporte, conquistar muitos sonhos. Hoje estamos materializando mais um por meio da Lei de Incentivo ao Esporte (LIE), que nos oportuniza estender a estas crianças tudo aquilo que aprendemos com a prática desta arte marcial. Colhemos depoimentos muito importantes com as famílias que fazem parte do projeto sobre mudanças comportamentais na escola e em casa. Tudo isso só reforça a convicção de que seguimos no caminho certo.” </p> 
+
+<p>A representante da Comunidade Tri, Claudia Bueno, agradeceu aos que estiveram “dispostos a ver o projeto sob um olhar diferente:” “Estas crianças irão se tornar referência para muitas outras, nos permitindo voar mais alto. Nosso muito obrigado ao Grêmio, a todos os parceiros aqui presentes e a esta comunidade tão carente e que está sendo premiada com o projeto Futuro Suave do Gramado ao Tatame. Eu vivi para viver tudo isso e sou muito grata por tanto”, destacou Claudia, em uma fala emocionada. </p> 
+
+<p>O encerramento da cerimônia contou com a palavra do mandatário gremista Romildo Bolzan, que também se mostrou satisfeito em poder entregar à comunidade um projeto lincado ao esporte. “O Grêmio se sente muito honrado em participar de um projeto desta magnitude. Os alunos têm uma oportunidade fantástica de autodesenvolvimento e quem sabe até de despertar talentos promissores. Servir como ponte para realizações como as que foram postas aqui é, sem dúvida, motivo de muito orgulho para todos nós”, disse ele. </p>   
+
+<p>Ao final, professores e alunos realizaram uma demonstração da prática esportiva, num tatame posicionado em frente aos convidados. </p> 
+
+<p>Também estiveram presentes à solenidade, o Secretário Adjunto da Secretaria Municipal de Esporte, Lazer e Juventude, Nelson Beron,  a Subprefeita do Bairro Humaitá, Tenente Daniela Patussi, o procurador geral de Porto Alegre, Dr. Roberto Rocha, o presidente da Arena do Grêmio, Mauro Araújo, diretores presidentes da empresa Bondamann, Ernani Follmann e Willian Bond, além do Major Gelson, Secretário Municipal adjunto de Segurança e Vinícius Kaster do Conselho Regional de Educação Física. O evento foi acompanhado também  pelo campeão mundial de judô, João Derly, w por Robinson Viana,   representando a ABT Materiais Elétricos, patrocinadora do projeto. </p> 
+
+<p>O Projeto </p> 
+
+<p>Com o objetivo de atender crianças que vivem em situação de vulnerabilidade social a ter um futuro mais digno, tendo o esporte como princípio de vida, e ao mesmo tempo ajudar a evitar a evasão escolar, o Grêmio lançou o projeto “Futuro Suave do Gramado ao Tatame”. </p> 
+
+<p>Criado no início deste ano, por meio de uma parceria com a Associação de Gerenciamento de Projetos Sociais (AGPS), o projeto visa reunir crianças entre 7 e 15 anos, às terças e quintas-feiras, em horários inverso ao da escola regular, para aulas de Jiu-Jitsu. As atividades, que terão como sede o hall da Tribuna de Honra do Conselho Deliberativo da Arena, seguirão até dezembro de 2022, e serão ministradas por sete professores especializados na arte. </p> 
+
+
+<?php 
+include_once 'footer.php';
+?>
---
+++ noticiaum.php
@@ -0,0 +1,60 @@
+<?php 
+include_once 'navbar.php';
+?>
+
+<div id="textoum">
+<h2>Com gols de Diego Souza e Janderson, Grêmio vence Novorizontino na Arena</h2>
+</div>
+<div id="textodois">
+<h3>Equipe gremista encerra 11ª rodada na quinta colocação da Série B do Brasileiro</h3>
+</div>
+<div id="textotres">
+<p> 07 JUN 2022 23:29 | ATUALIZADO EM 07 JUN 2022 23:47</p>
+</div>
+<div id="imagemum">
+<img src="imagens/noticiagremio.jpg">
+</div>
+
+<div id="textoquatro">
+<p>Com o apoio da torcida tricolor na Arena, que apoiou durante os 90 minutos, o Grêmio conquistou os três pontos no Campeonato Brasileiro Série B, diante do Novorizontino. A partida, que foi disputada na noite desta terça-feira, 7, terminou com o placar de 2 a 0 para a equipe de Roger Machado. Os gols foram marcados por Diego Souza e Janderson. </p>
+
+
+<p>Com o resultado, a equipe gremista encerra a 11ª rodada na quinta colocação do campeonato, com 17 pontos. </p>
+
+<p>Dentro de campo, o técnico Roger Machado manteve a formação da equipe com três zagueiros, mas contou com desfalques. Na defesa, Bruno Alves com sintomas gripais e Brenno, que sentiu um desconforto na panturrilha durante o aquecimento, estiveram fora da partida. No ataque, Janderson entrou na vaga de Benitez, lesionado. </p>
+
+<p>Nos primeiros minutos, o Grêmio encontrou um adversário retrancado, mas a marcação não mudou a postura do Tricolor, que trocava passes e priorizava a posse de bola, em busca o ataque. O primeiro lance de perigo da partida veio aos sete minutos, a favor da equipe de Roger Machado. Edilson cruzou na cabeça de Diego Souza, que finalizou, mas a bola acabou desviando na defesa, e passou perto da trave. </p>
+
+<p>Com nove minutos jogados, foi a vez de Biel tentar abrir o marcador. O atacante finalizou de fora de área, mas a bola subiu demais. Quatro minutos depois, Bitello chutou da entrada da grande área, porém sem perigo para o goleiro adversário, que viu a bola sair pela linha de fundo. </p>
+
+<p>Todas as chances da partida eram a favor do Grêmio, Gabriel Grando mal trabalhava. Aos 16’, Nicolas cobrou falta pelo lado esquerdo na cabeça de Rodrigues, e, na finalização, faltou pouco para a bola entrar. A pressão seguiu. Com 23’ jogados, a equipe de Roger Machado teve a melhor chance do jogo. Janderson avançou pela direita, cruzou e, na finalização, Bitello chutou forte, fazendo o goleiro Giovanni realizar uma grande defesa. No rebote, Biel quase empurrou a bola para o fundo das redes. </p> 
+
+<p>Aos 32’, a insistência parecia ter dado resultado. O gol saiu, mas a arbitragem viu impedimento de Bitello, que havia concluído para gol.  </p>
+
+<p>A primeira vez que Gabriel Grando precisou trabalhar, foi aos 38’. Ronaldo cabeceou na entrada da pequena área, nas mãos do goleiro gremista, sem grande perigo. </p>
+
+<p>No último minuto da primeira etapa, lances polêmicos aconteceram. Primeiro, Diego Souza chutou e a finalização pegou no zagueiro Rodolfo, fazendo os gremistas pedirem pênalti. O árbitro mandou o jogo seguir. Logo depois, a bola bateu no mesmo jogador, após um chute de Geromel, e a arbitragem confirmou a penalidade no VAR. Diego Souza cobrou e abriu o placar na Arena. </p> 
+
+<p>O Grêmio retornou para a segunda etapa sem modificações e, assim como na primeira, os minutos iniciais foram de muito toque de bola do Tricolor. O ponteiro passava dos 10’, e nenhuma finalização era contabilizada, mas na primeira oportunidade, aos 13’, o Grêmio ampliou o marcador. A jogada começou com Nicolas, que tocou para Biel e ela acabou sobrando para Janderson, que estufou as redes. </p>
+
+<p>Roger Machado realizou a primeira alteração na equipe, aos 16’. Diego Souza saiu para entrada de Elkeson. Três minutos depois, Biel pifou Janderson que avançou pela direita e bateu cruzado, para a grande defesa do goleiro adversário. </p>
+
+<p>Na frente do placar, o Grêmio seguiu pressionando e levando perigo à meta de Giovanni. Com 22’, Elkeson cabeceou, após cobrança de escanteio, mas a bola foi para fora. </p> 
+
+<p>Aos 25’, mais trocas aconteceram no Tricolor. Lucas Silva e Elias entraram nas vagas de Thiago Santos e Biel. A partida deu uma esfriada nos minutos finais e fez Roger Machado modificar, pela última vez, a equipe. Sarará e Gabriel Silva foram os escolhidos para entrarem nos lugares de Bitello e Janderson. </p>
+
+<p>O Novorizontino até tentou pressionar a defesa gremista no final do jogo, mas não levou dificuldades para Grando. </p>
+
+<p>Final de jogo: Grêmio 2x0 Novorizontino </p>
+
+<p>O próximo desafio do Grêmio será na próxima segunda-feira, 13, quando enfrenta o Sport, em Recife. </p>
+</div>
+
+
+
+
+
+
+<?php 
+include_once 'footer.php';
+?>
