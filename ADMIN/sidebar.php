80e0c440f196b6d5cfce5b5c6cbeb73cc0e33296
yL0pes <lopeshenrique538@gmail.com>
2022-06-20 09:17:46 -0300

    Admin page

---
+++ ADMIN/sidebar.php
@@ -0,0 +1,90 @@
+<div class="dropdown-menu dropdown-menu-arrow dropdown-menu-right">
+            <div class=" dropdown-header noti-title">
+              <h6 class="text-overflow m-0">Bem-Vindo!</h6>
+            </div>
+            <a href="./examples/profile.php" class="dropdown-item">
+              <i class="ni ni-single-02"></i>
+              <span>Meu Perfil</span>
+            </a>
+            <a href="./examples/profile.php" class="dropdown-item">
+              <i class="ni ni-settings-gear-65"></i>
+              <span>Configurações</span>
+            </a>
+            <a href="./examples/profile.php" class="dropdown-item">
+              <i class="ni ni-calendar-grid-58"></i>
+              <span>Atividades</span>
+            </a>
+            <a href="./examples/profile.php" class="dropdown-item">
+              <i class="ni ni-support-16"></i>
+              <span>Suporte</span>
+            </a>
+            <div class="dropdown-divider"></div>
+            <a href="#!" class="dropdown-item">
+              <i class="ni ni-user-run"></i>
+              <span>Sair</span>
+            </a>
+          </div>
+        </li>
+      </ul>
+      <!-- Collapse -->
+      <div class="collapse navbar-collapse" id="sidenav-collapse-main">
+        <!-- Collapse header -->
+        <div class="navbar-collapse-header d-md-none">
+          <div class="row">
+            <div class="col-6 collapse-brand">
+              <a href="./index.html">
+                <img src="./assets/img/brand/blue.png">
+              </a>
+            </div>
+            <div class="col-6 collapse-close">
+              <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#sidenav-collapse-main" aria-controls="sidenav-main" aria-expanded="false" aria-label="Toggle sidenav">
+                <span></span>
+                <span></span>
+              </button>
+            </div>
+          </div>
+        </div>
+        <!-- Form -->
+        <form class="mt-4 mb-3 d-md-none">
+          <div class="input-group input-group-rounded input-group-merge">
+            <input type="search" class="form-control form-control-rounded form-control-prepended" placeholder="Search" aria-label="Search">
+            <div class="input-group-prepend">
+              <div class="input-group-text">
+                <span class="fa fa-search"></span>
+              </div>
+            </div>
+          </div>
+        </form>
+        <!-- Navigation -->
+        <ul class="navbar-nav">
+          <li class="nav-item  active ">
+            <a class="nav-link  active " href="./index.html">
+              <i class="ni ni-tv-2 text-primary"></i> Dashboard
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link " href="./examples/icons.html">
+              <i class="ni ni-planet text-blue"></i> Icones
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link " href="./examples/maps.html">
+              <i class="ni ni-pin-3 text-orange"></i> Mapas
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link " href="./examples/profile.html">
+              <i class="ni ni-single-02 text-yellow"></i> Perfil de Usuário
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link " href="./examples/tables.html">
+              <i class="ni ni-bullet-list-67 text-red"></i> Tabelas
+            </a>
+          </li>
+        </ul>
+        <!-- Divider -->
+        <hr class="my-3">
+      </div>
+    </div>
+  </nav>
