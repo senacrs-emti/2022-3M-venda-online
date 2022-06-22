26739dcba401cd464002eb869d89b3c5d14bdfe8
VicenzoMF <vicenzomf@gmail.com>
2022-06-21 23:10:24 -0300

    commit

--- Login/login.php
+++ Login/login.php
@@ -1,51 +1,63 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-	<title>Login</title>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-	<link href="https://fonts.googleapis.com/css2?family=Jost:wght@500&display=swap" rel="stylesheet">
-	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-    <link rel="stylesheet" href="login.css">
-	<link rel="icon" href="images/gremio.png">
+  <title>Login</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+    integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <link href="https://fonts.googleapis.com/css2?family=Jost:wght@500&display=swap" rel="stylesheet">
+  <link rel="stylesheet"
+    href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+  <link rel="stylesheet" href="login.css">
+  <link rel="icon" href="images/gremio.png">
 </head>
+
 <body>
-	
-	<div class="main">
 
-		<input type="checkbox" id="chk" aria-hidden="true" onchange="transformPopUp(this)">
+  <div class="main">
 
-			<div class="signup"> <!-- Cadastro de Usuário!-->
-				<div for="chk" class="popup-user" id="popupUser" >
-					<span class="close-button material-symbols-outlined" onclick="closePopupUser()">close</span>
-					<span id="innerText"></span>
-				</div>
-				<form method="POST" action="">
+    <input type="checkbox" id="chk" aria-hidden="true" onchange="transformPopUp(this)">
 
-					<label for="chk" aria-hidden="true">Cadastrar</label>
-					<input type="text" id="NomeDeUsuario" name="NomeDeUsuario" placeholder="Nome de Usuário" required>
-					<input type="Email" id="Email" name="Email" placeholder="Email" required>
-					<input type="password" id="Senha" name="Senha" placeholder="Senha"  required>
-					<input type="file" id="foto" accept="image/*"	name="foto" placeholder="foto" required>
-					<button id="btCadastro">Cadastro</button>
-				</form>
-			</div>
- 
-			<div class="login"> <!-- Login de Usuário!-->
-				<form method="POST" action="Perfil/index.php">
-					<label for="chk" aria-hidden="true">Login</label>
-					<input type="email" name="Email" placeholder="Email" required>
-					<input type="password" name="Senha" placeholder="Senha" required>
-					<button id="login" name="login">Login</button>
-				</form>
-			</div>
-	</div>
-	<script>
-	</script>
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-	<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
-	<script src="../script.js"></script>
+    <div class="signup">
+      <!-- Cadastro de Usuário!-->
+      <div for="chk" class="popup-user" id="popupUser">
+        <span class="close-button material-symbols-outlined" onclick="closePopupUser()">close</span>
+        <span id="innerText"></span>
+      </div>
+      <form method="POST" action="">
+
+        <label for="chk" aria-hidden="true">Cadastrar</label>
+        <input type="text" id="NomeDeUsuario" name="NomeDeUsuario" placeholder="Nome de Usuário" required>
+        <input type="Email" id="Email" name="Email" placeholder="Email" required>
+        <input type="password" id="Senha" name="Senha" placeholder="Senha" required>
+        <button id="btCadastro">Cadastro</button>
+      </form>
+    </div>
+
+    <div class="login">
+      <!-- Login de Usuário!-->
+      <form method="POST" action="Perfil/index.php">
+        <label for="chk" aria-hidden="true">Login</label>
+        <input type="email" name="Email" placeholder="Email" required>
+        <input type="password" name="Senha" placeholder="Senha" required>
+        <button id="login" name="login">Login</button>
+      </form>
+    </div>
+  </div>
+  <script>
+  </script>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+  </script>
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+    integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
+  </script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+    integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
+  </script>
+  <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+  <script src="../script.js"></script>
 </body>
+
 </html>
--- script.js
+++ script.js
@@ -1,95 +1,88 @@
-const popupUser = document.getElementById("popupUser");
-const innerText = document.getElementById("innerText");
-const closeButton = document.getElementById("close-button");
+const popupUser = document.getElementById('popupUser')
+const innerText = document.getElementById('innerText')
+const closeButton = document.getElementById('close-button')
 
-popupUser.style.display = "none";
+popupUser.style.display = 'none'
 
 //create cookie function
 function setCookie(cname, cvalue, exdays) {
-  const d = new Date();
-  d.setTime(d.getTime() + exdays * 60 * 1000);
-  let expires = "expires=" + d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires;
+  const d = new Date()
+  d.setTime(d.getTime() + exdays * 60 * 1000)
+  let expires = 'expires=' + d.toUTCString()
+  document.cookie = cname + '=' + cvalue + ';' + expires
 }
 
 //get cookie function
 function getCookie(cname) {
-  let name = cname + "=";
-  let ca = document.cookie.split("=");
+  let name = cname + '='
+  let ca = document.cookie.split('=')
 
   for (let i = 0; i < ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) == " ") {
-      c = c.substring(1);
+    let c = ca[i]
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1)
     }
     if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
+      return c.substring(name.length, c.length)
     }
   }
-  return ca[1];
+  return ca[1]
 }
 
 $(document).ready(function () {
-  let cookieRespostaSlice = getCookie(cookieResposta);
-  var cookieResposta = cookieRespostaSlice.slice(";")[0];
-  console.log(cookieResposta);
-  
-  if (cookieResposta == "1") {
-    popupUser.style.display = "flex";
-    innerText.innerHTML = "Usuário já existente.";
+  let cookieRespostaSlice = getCookie(cookieResposta)
+  var cookieResposta = cookieRespostaSlice.slice(';')[0]
+  console.log(cookieResposta)
+
+  if (cookieResposta == '1') {
+    popupUser.style.display = 'flex'
+    innerText.innerHTML = 'Usuário já existente.'
   } else {
-    if (cookieResposta == "2") {
-      popupUser.style.display = "flex";
-      innerText.innerHTML = "Email já existente.";
-    } else { if (cookieResposta == '3') {
-      popupUser.style.display = "flex";
-      innerText.innerHTML = "Cadastrado com sucesso!";
-      popupUser.style.background = "green"
+    if (cookieResposta == '2') {
+      popupUser.style.display = 'flex'
+      innerText.innerHTML = 'Email já existente.'
     } else {
-
     }
-    }
-  } 
+  }
 
-  $("#btCadastro").click(function () {
-    var Email = $("#Email").val();
-    var NomeDeUsuario = $("#NomeDeUsuario").val();
-    var Senha = $("#Senha").val();
+  $('#btCadastro').click(function () {
+    var Email = $('#Email').val()
+    var NomeDeUsuario = $('#NomeDeUsuario').val()
+    var Senha = $('#Senha').val()
 
     $.ajax({
-      url: "./cadastro.php",
-      type: "POST",
+      url: './cadastro.php',
+      type: 'POST',
       data:
-        "Email=" +
+        'Email=' +
         Email +
-        "&NomeDeUsuario=" +
+        '&NomeDeUsuario=' +
         NomeDeUsuario +
-        "&Senha=" +
+        '&Senha=' +
         Senha,
-      dataType: "html",
+      dataType: 'html'
     })
       .done(function (resposta) {
-        console.log(resposta);
-        setCookie("cookieResposta", resposta, 1);
-
+        console.log(resposta)
+        setCookie('cookieResposta', resposta, 1)
       })
       .fail(function (jqXHR, textStatus) {
-        console.log("Request failed: " + textStatus);
+        console.log('Request failed: ' + textStatus)
       })
       .always(function () {
-        console.log("completou");
-      });
-  });
-});
+        console.log('completou')
+      })
+  })
+})
 
 function transformPopUp() {
   if (chk.checked == true) {
-    document.getElementById("popupUser").style.transform = "translateY(230%)";
+    document.getElementById('popupUser').style.transform = 'translateY(230%)'
   } else {
-    document.getElementById("popupUser").style.transform = "translateY(100%)";
+    document.getElementById('popupUser').style.transform = 'translateY(100%)'
   }
 }
 
 function closePopupUser() {
-  popupUser.style.display = "none";
+  popupUser.style.display = 'none'
 }
